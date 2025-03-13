f42d3937197651edda6af89482cd9c09aa4eddb2
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-03-13 08:50:47 +0600

    movies list show

---
+++ lib/screens/my_home_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/data/movies.dart';
+import 'package:movie_app/model/movie.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Movie> movieList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    showMovies();
+  }
+
+  void showMovies() {
+    List movies = MovieList.movies;
+    setState(() {
+      movieList = movies.map((movie) => Movie.fromJson(movie)).toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: const Text("Movie List"),
+      ),
+      body: ListView.builder(
+        itemCount: movieList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(movieList[index].title),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
